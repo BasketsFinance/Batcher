e389ca9a279620fac66b1f223da279cf9346a7c9
baskets-finance <baskets.finance@gmail.com>
2021-04-17 10:25:52 +0300

    solidity version changed

--- truffle-config.js
+++ truffle-config.js
@@ -35,7 +35,7 @@ module.exports = {
 
     compilers: {
         solc: {
-            version: '0.6.12',
+            version: '0.5.16',
             settings: {
                 optimizer: {
                     enabled: true,
